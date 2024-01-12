3d4b99a08cac79103d1b90fee315313f4196aa5b
Matias Sepulveda <ezesepulveda444@gmail.com>
2024-01-11 22:21:25 -0300

    Salto, Impulso, animaciones

--- Assets/Scripts/Interactive/Impulse.cs
+++ Assets/Scripts/Interactive/Impulse.cs
@@ -5,47 +5,53 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 
-public class Impulse : MonoBehaviour, IObserverImpulse
+public class Impulse : MonoBehaviour, IObserverBoost
 {
     [SerializeField] ForceMode2D _impulseMode = ForceMode2D.Impulse;
     [SerializeField] Renderer _renderer;
-    [Range(0,20)]
-    [SerializeField] int _force;
-    [Tooltip("Activacion")]
-    [SerializeField] bool _active = true;
-    [Range(0, 3), Tooltip("Temporizador de activasion")]
-    [SerializeField] float _timmer = 2f;
+    [Range(100, 1000)]
+    [SerializeField] private int _force;
+    [SerializeField] private float _boostTime;
+    [SerializeField] private float _boostCooldown;
+    public bool _impulseEnabled;
 
+    private TrailRenderer _trailRenderer;
+    private Rigidbody2D _rb;
+    private Transform _transform;
 
     private void Start()
     {
+        _impulseEnabled = true;
         _renderer = GetComponent<Renderer>();
     }
 
-    public void Action (Rigidbody2D rb2d/*, Vector2 dir*/)
+    public void Boost(Rigidbody2D rb2d, Transform transform, TrailRenderer trailRenderer)
     {
-        if (_active)
-        {
-            Debug.Log("Impulso");
-            _force = Mathf.Clamp(_force,0,20);
-            rb2d.AddForce(Vector2.up * _force, _impulseMode);
-        }
+        _rb = rb2d;
+        _transform = transform;
+        _trailRenderer = trailRenderer;
 
-        StartCoroutine(Timer());
+        if (_impulseEnabled)
+        {
+            StartCoroutine(Boost());
+            Debug.Log("Impulso");
+        }
     }
 
-    IEnumerator Timer()
+    private IEnumerator Boost()
     {
-
-        _active = false;
+        _impulseEnabled = false;
+        //float originalGravity = _rb.gravityScale;
+        //_rb.gravityScale = 0f;
+        _rb.velocity = new Vector2(0f, _transform.localScale.y * _force);
+        _trailRenderer.emitting = true;
+        yield return new WaitForSeconds(_boostTime);
+        _trailRenderer.emitting = false;
+        //_rb.gravityScale = originalGravity;
         _renderer.material.color = Color.red;
-
-        yield return new WaitForSeconds(_timmer);
-
-        _active = true;
+        yield return new WaitForSeconds(_boostCooldown);
+        _impulseEnabled = true;
         _renderer.material.color = Color.yellow;
-
-        yield return null;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
